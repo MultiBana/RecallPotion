ebd7d04786a83ad28b62632f2c16d187a5852d3f
z3roco01 <zerocool@e.email>
2023-08-16 13:46:01 -0500

    Update to 1.20.1

--- src/main/java/com/multibana/recallpotion/ModPotions.java
+++ src/main/java/com/multibana/recallpotion/ModPotions.java
@@ -2,19 +2,19 @@ package com.multibana.recallpotion;
 
 import com.multibana.recallpotion.mixins.BrewingRecipeRegistryMixin;
 import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.Potions;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 
 public class ModPotions {
     public static Potion RECALL_POTION;
 	public static Potion ENDER_POTION;
 
 	public static Potion registerPotion(String name, StatusEffectInstance statusEffectInstance, Identifier identifier) {
-		return Registry.register(Registry.POTION, identifier,
+		return Registry.register(Registries.POTION, identifier,
 				new Potion(statusEffectInstance));
 	}
 	public static void registerPotions() {
